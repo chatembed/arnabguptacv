56c7be4bf3610f78769d46df21daee66b5695298
Arnab <ceo@chatembedai.com>
2025-10-29 23:11:36 +0530

    working

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
+// Hard-coded for GitHub Pages repo deployment
+// We serve the site at https://chatembed.github.io/arnabguptacv
 const repo = '/arnabguptacv';
+const absoluteAssetPrefix = 'https://arnabgupta.me';
 
 const nextConfig = {
   typescript: {
@@ -18,8 +21,9 @@ const nextConfig = {
   },
   output: 'export',
   trailingSlash: true,
+  // hard-coded basePath and assetPrefix for the GitHub Pages URL
   basePath: repo,
-  assetPrefix: repo,
+  assetPrefix: absoluteAssetPrefix,
 };
 
 export default nextConfig;
